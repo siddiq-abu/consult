dd0c09b80ac12f80b2ff791c3c61bf9ae5e15e4e
siddiq-abu <abusiddiq200g3@gmail.com>
2023-11-14 10:58:45 +0000

    update

--- assets/app.js
+++ assets/app.js
@@ -15,17 +15,45 @@ let swiper = new Swiper(".mySwiper", {
     },
   });
 
-  // 
 
-  // var swiperTwo = new SwiperTwo(".mySwiperTwo", {
-  //   cssMode: true,
-  //   navigation: {
-  //     nextEl: ".swiper-button-next",
-  //     prevEl: ".swiper-button-prev",
-  //   },
-  //   pagination: {
-  //     el: ".swiper-pagination",
-  //   },
-  //   mousewheel: true,
-  //   keyboard: true,
-  // });
+// /* When the user clicks on the button, 
+// toggle between hiding and showing the dropdown content */
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }
+
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
+
+/// show hide nav menu
+
+const menu = document.querySelector(".nav-links");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click',() => {
+  menu.style.display = "flex";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+})
+
+
+// close nav menu
+const closeNav = () => {
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click',closeNav)
